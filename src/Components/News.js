64476044bb15218b77273420bcf213a7c5bf2638
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-13 14:08:11 +0530

    Prev and Next Button Added with Page Size Count

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,35 +3,95 @@ import React, { Component } from "react";
 import NewsItems from "./NewsItems";
 
 export class News extends Component {
-    
-    constructor(){
-        super()
-        this.state={
-            articles:[],
-            loading : false
-        }
-    }
-    async componentDidMount(){
-      //console.log('cdm');
-      let url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd"
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+  async componentDidMount() {
+    //console.log('cdm');
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=1";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    //console.log(data);
+    //console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+
+  handlePrevClick = async () => {
+    console.log("Prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    //console.log(data);
+    //console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=${
+        this.state.page + 1
+      }&pageSize=20`;
       let data = await fetch(url);
-      let parseData = await data.json()
-      //console.log(data);
-      //console.log(parseData);
-      this.setState({articles : parseData.articles})
-      
+      let parsedData = await data.json();
+      console.log(data);
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
     }
+  };
+
   render() {
     return (
       <div className="container my-3">
-        <h2>News Monkey - Headline</h2>
+        <h2>News Monkey - Top Headlines</h2>
         <div className="row">
-        {this.state.articles.map((element)=>{
-          return <div className="col md-4">
-          <NewsItems key={element.url} title={element.title.slice(0,45)} description={element.description.slice(0,88)} imageUrl={element.urlToImage} newsUrl={element.url} />
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col   md-4" key={element.url}>
+                <NewsItems
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
         </div>
-        })}
-          
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <button
+          disabled={this.state.page >= this.state.totalResults / 20}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
         </div>
       </div>
     );
--- src/Components/NewsItems.js
+++ src/Components/NewsItems.js
@@ -7,13 +7,12 @@ export class NewsItems extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img src={!imageUrl?"https://www.breezemasti.com/wp-content/uploads/Nithya-Menen-Actress-wiki-age-family-husband-movies.jpg": imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <a href={newsUrl} target='blank' className="btn btn-sm btn-primary">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a rel="noreferrer" href={newsUrl} target='blank' className="btn btn-sm btn-primary">
               Read More
-              
             </a>
           </div>
         </div>

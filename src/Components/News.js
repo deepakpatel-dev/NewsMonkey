8fe41159a8d53a30a48f103c39b6019d33e78779
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-13 17:25:36 +0530

    Adding loading spinner & variable pageSize

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,20 +1,22 @@
 import React, { Component } from "react";
 //import PropTypes from "prop-types";
 import NewsItems from "./NewsItems";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
     super();
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
     };
   }
   async componentDidMount() {
     //console.log('cdm');
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=1";
+      `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
     //console.log(data);
@@ -22,14 +24,14 @@ export class News extends Component {
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading:false
     });
   }
 
   handlePrevClick = async () => {
     console.log("Prev");
-    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=${
-      this.state.page - 1
-    }&pageSize=20`;
+    let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
     //console.log(data);
@@ -37,15 +39,14 @@ export class News extends Component {
     this.setState({
       page: this.state.page - 1,
       articles: parsedData.articles,
+      loading:false
     });
   };
   handleNextClick = async () => {
     console.log("next");
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-    } else {
-      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=${
-        this.state.page + 1
-      }&pageSize=20`;
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults /this.props.size))) {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=4eae62a8157545deb7af46399afb93dd&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+      this.setState({loading:true});
       let data = await fetch(url);
       let parsedData = await data.json();
       console.log(data);
@@ -53,6 +54,7 @@ export class News extends Component {
       this.setState({
         page: this.state.page + 1,
         articles: parsedData.articles,
+        loading:false
       });
     }
   };
@@ -60,11 +62,12 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-3">
-        <h2>News Monkey - Top Headlines</h2>
+        <h2 className="text-center">News Monkey - Top Headlines</h2>
+          {this.state.loading&&<Spinner/>}
         <div className="row">
-          {this.state.articles.map((element) => {
+          {!this.state.loading&&this.state.articles.map((element) => {
             return (
-              <div className="col   md-4" key={element.url}>
+              <div className="col md-4" key={element.url}>
                 <NewsItems
                   title={element.title ? element.title : ""}
                   description={element.description ? element.description : ""}
@@ -85,7 +88,7 @@ export class News extends Component {
             &larr; Previous
           </button>
           <button
-          disabled={this.state.page >= this.state.totalResults / 20}
+          disabled={this.state.page >= this.state.totalResults / this.props.pageSize}
             type="button"
             className="btn btn-dark"
             onClick={this.handleNextClick}

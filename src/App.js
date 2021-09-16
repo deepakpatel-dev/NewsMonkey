85e19f72760544a1836d9385c70f538dce4c7462
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-09-16 11:39:51 +0530

    Toggle (Dark/Light)Mode Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./Components/Navbar";
 import News from "./Components/News";
 import {BrowserRouter as Router,Switch,Route,} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar'
+//import PropTypes from 'prop-types'
+
 
 var today = new Date();
 var str = today.toGMTString();
@@ -12,11 +14,26 @@ var str = today.toGMTString();
    const pageSize = 9;
    const apiKey=process.env.REACT_APP_NEWS_API
    const [progress, setProgress] = useState(0)
+   const [mode, setMode] = useState('light')
   
+   const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      //showAlert('Dark Mode has been enabled' ,'success');
+      // document.title = 'TextUtils - Dark Mode';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      //showAlert('Light Mode has been enabled' ,'success');
+      // document.title = 'TextUtils - Light Mode';
+    }
+  }
     return (
       <div>
         <Router>
-        <Navbar />
+        <Navbar mode={mode} toggleMode={toggleMode}/>
         <LoadingBar color='#f11946' height={3} progress={progress} />
         <Switch>
         <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey}  key="general" pageSize={pageSize} country='in' category='general'/></Route>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
+//import PropTypes from 'prop-types'
+
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = (props) => {
 
   return (
     <div>
-      <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+      <nav className={`navbar fixed-top navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">NewsMonkey</Link>
           <button className="navbar-toggler" type="button"data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
@@ -27,6 +29,10 @@ const Navbar = () => {
               <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
               <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
             </ul>
+            <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+                        <input className="form-check-input" onClick={props.toggleMode} type="checkbox" id="flexSwitchCheckDefault"/>
+                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+                    </div>
           </div>
         </div>
       </nav>
